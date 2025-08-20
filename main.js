af894fc3d16c3a1f738ace8b1aaa646f4cbf9477
Hriday Kundu <hridayk831@gmail.com>
2025-08-20 21:15:27 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,62 @@
+//**** function on navbar ********//
+
+window.addEventListener('scroll', () => { 
+    document.querySelector('nav').classList.toggle('window-scroll', window.scrollY > 0)
+})
+
+//*****faq answer show/hide******* *//
+const faqs = document.querySelectorAll('.faq');
+
+faqs.forEach( faq => {
+    faq.addEventListener('click', () => {
+        faq.classList.toggle('open');
+
+        //change icon
+        const icon = faq.querySelector('.faq__icon i');
+        if(icon.className == 'uil uil-plus'){
+            icon.className = "uil uil-minus"
+        } else {
+            icon.className = "uil uil-plus"
+        }
+    })
+})
+
+// show/hide nav manu
+const menu = document.querySelector(".nav__menu");
+const menuBtn = document.querySelector("#open-menu-btn");
+const closeBtn = document.querySelector("#close-menu-btn");
+
+menuBtn.addEventListener('click', () => {
+    menu.style.display = "flex";
+    closeBtn.style.display = "inline-block";
+    menuBtn.style.display = "none";
+});
+
+// close nav manu
+const closeNav = () => {
+    menu.style.display = "none";
+    closeBtn.style.display = "none";
+    menuBtn.style.display = "inline-block";
+}
+
+closeBtn.addEventListener('click', closeNav)
+
+
+
+
+
+//========== login page part start ===================//
+
+const container = document.querySelector('.container');
+const registerBtn = document.querySelector('.register-btn');
+const loginBtn = document.querySelector('.login-btn');
+
+registerBtn.addEventListener('click', () => {
+    container.classList.add('active');
+});
+
+loginBtn.addEventListener('click', () => {
+    container.classList.remove('active');
+});
+
+//========== login page part end ===================//
